b8d0d64d948809c5ceeb3d84d9b7bfe20d9a58c8
SimonRollus <simrol260@gmail.com>
2022-05-06 15:07:11 +0200

    Just a test : les placeholder sont là pour les packages s'affichent sur GITHUB

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,10 @@
 package model;
 
 public class User {
+    private Integer id;
+    private String username;
+    //Role
+    private String lastName;
+    private String firstName;
+    private String phoneNumber;
 }
